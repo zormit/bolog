bad1d1a9109e42c2471618ecc64b09195afd5821
Moritz Neeb <moritz.neeb@campus.tu-berlin.de>
2017-02-23 11:30:07 -0500

    basic setup: book MVC
    * following the getting started tutorial http://guides.rubyonrails.org/getting_started.html
    * the book model has three string based attributes:
        * Title
        * Author
        * ISBN
    * the controller allows for basic CRUD
    * the view is super minimal and ugly

---
+++ README.md
@@ -0,0 +1,18 @@
+# bolog
+
+This is a book catalog written in Ruby on Rails.
+
+
+## bootstrap
+
+* Ruby version: ruby 2.3.3
+
+* System dependencies: none?
+
+* Configuration: none?
+
+* Database creation: bin/rails db:migrate?
+
+* Database initialization: ...
+
+* How to run the test suite: No tests written yet.
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret",
+                               except: [:index, :show]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :isbn)
+    end
+end
