50a30deb22c5dc87df6b6bdce054b7d72d9ba1c9
Moritz Neeb <(none)>
2017-03-06 15:02:48 -0500

    add capybara dependency
    see https://github.com/teamcapybara/capybara#setup

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
   gem 'rails-controller-testing'
+  gem 'capybara'
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@
 # The generated `.rspec` file contains `--require spec_helper` which will cause
 # this file to always be loaded, without a need to explicitly require it in any
 # files.
-#
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
@@ -11,6 +10,10 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+#
+#
+# Add capybara here for now, despite the recommentation:
+require 'capybara/rspec'
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
